9c03f749ba3d6e2fed7852621e2ffc48735ac4e3
HarpreetGh <70241088+HarpreetGh@users.noreply.github.com>
2020-12-07 19:38:02 -0800

    Update
    Co-Authored-By: csmer <70241048+csmer@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <array>
+#include <algorithm>
+#include <cmath>
+#include <random>
 //#include <bits/stdc++.h> 
 
 using namespace std;
@@ -62,10 +66,77 @@ class Grid {
         }
     }
 
-    void randomizeArray(){
+    void randomize(){
+        cout<< "Hello World";
+        array<int, 9> shuffleArray;
+
+        //Iterate through 1..8 to populate shuffleArray with 1,2,3..8
+        for(int i = 0; i < 9; i++){
+            shuffleArray[i] = i+1;
+        }
+        unsigned seed = 0;
+       // cout << "Seed: ," << seed;
+        for(int i = 0; i < 9; i++){
+        cout << shuffleArray.at(i) << "";
+        }
+
+        cout << shuffleArray.at(1);
         
-        //random_shuffle(arr.begin(), arr.end());
+        shuffle(shuffleArray.begin(), shuffleArray.end(), default_random_engine(seed));
+        for(int i = 0; i < 9; i++){
+        cout << shuffleArray.at(i) << "";
+        }
     }
+
+/*
+    void randomize(){
+        //vector<int> randomArray(gridSize*gridSize);
+        
+        //Init array called shuffleArray with nodeNum which is 9 (gridsize of 9).
+        cout << "Reached Randomize function" << endl;
+        array<int, 9> shuffleArray;
+
+        //Iterate through 1..9 to populate shuffleArray with 1,2,3..9
+        for(int i = 0; i < nodeNum; i++){
+            shuffleArray[i] = i+1;
+        }
+        unsigned seed = 0;
+        cout << "Seed: " << seed << endl;
+        shuffle(shuffleArray.begin(), shuffleArray.end(), default_random_engine(seed));
+        //shuffleArray.size();
+        for(int i = 0; i < 9; i++){
+        cout << shuffleArray.at(i) << "";
+        }
+        //[1..nodeNum-1] <- shuffle that. Then swap the index of matrix with that shuffle
+        //example: shuffled: [5,2,7,4,8,6,3,9,1]
+        //ex:          grid: [1,2,3,4,5,6,7,8,9]
+
+        
+    }
+
+    void Shuffle(){
+        int newShuffle[9] = {1,2,3,4,5,6,7,8,9}; 
+        //int newShuffle[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        //int newShuffle[9] {8,9,7,1,2,4,6,5,3};
+        int original[9] =   {1,2,3,4,5,6,7,8,9};
+
+        for(int i = 0; i < 9; i++){
+            Swap(newShuffle[i], original[i]);
+        }
+    }*/
+
+    void Swap(int newLoc, int oldLoc){
+        //locations being 1-9 as the spots on grid of 3x3
+        int newLocX = floor(newLoc / (gridSize + 0.1));
+        int newLocY = (newLoc - 1) % gridSize;
+        int oldLocX = floor(oldLoc / (gridSize + 0.1));
+        int oldLocY = (oldLoc - 1) % gridSize;
+        
+        string temp = gridNodes[oldLocX][oldLocY].number;
+        gridNodes[oldLocX][oldLocY].number = gridNodes[newLocX][newLocY].number;
+        gridNodes[newLocX][newLocY].number = temp;
+    }
+
 
     
 };
@@ -75,5 +146,8 @@ class Grid {
 int main()
 {
     Grid a;
+    a.PrintGrid();
+    cout << endl;
+    //a.Shuffle();
     a.PrintGrid();
 }
